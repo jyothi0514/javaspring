c66b6e104730662554aa0576e9f2787f4a5658df
jyothi0514 <107556723+jyothi0514@users.noreply.github.com>
2022-06-16 18:11:28 +0530

    Create Add.java

---
+++ Add.java
@@ -0,0 +1,12 @@
+// Addition of two numbers
+
+class x1
+{
+        public static void main(String args[]) {
+                int a,b,c;
+                a=23;
+                b=45;
+                c=a+b;
+                System.out.println(c);
+        }
+}
